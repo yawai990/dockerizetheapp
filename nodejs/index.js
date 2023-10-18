78db94afccdafd798186ba502bb0e4199895251c
yawai990 <ywaung990@gmail.com>
2023-10-18 16:57:43 +0630

    twilio added

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const dontenv = require("dotenv");
 const app = express();
 const PORT = 8080;
 const HOST = "0.0.0.0";
+
+dontenv.config();
 
 app.get("/", (req, res, next) => {
   res.send("hello node js");
@@ -14,6 +17,19 @@ app.get("/greeting", (req, res, next) => {
     school: "bhonekyikyaung",
   });
 });
+
+const twilioClient = require("twilio")(
+  process.env.TWILIO_ACCOUNDID,
+  process.env.TWILIO_AUTHTOKEN
+);
+
+twilioClient.messages
+  .create({
+    body: "hello from twilio", // a message text
+    to: "phone number", // phone no that you want to send a message
+    from: "phone number", //from valid twilio number
+  })
+  .then((message) => console.log(message.accountSid));
 
 app.listen(PORT, HOST, () => {
   console.log("server run");
